809a1d1466b518217815a28361d6aa09a1ee6e8d
eden_gertsman <Gamma-user51@greendreamteam.onmicrosoft.com>
2025-02-16 14:02:26 +0200

    Documentation and files order

---
+++ algo/Algorithms/RoundRobin.py
@@ -0,0 +1,55 @@
+import time
+from utils import Junction, TrafficLight
+from typing import List
+
+
+class RoundRobinController:
+    """
+    Controls traffic lights in a junction
+    """
+
+    def __init__(self, junction: Junction):
+        """
+        Constructor
+        :param junction: object that represents a junction
+        """
+
+        self.junction = junction
+        self.current_traffic_light_id = self.junction.get_traffic_lights()[0].id
+        self.time_interval = 5
+
+    def start(self):
+        """
+        Starts the algorithm
+        """
+
+        while True:
+            lights: List[TrafficLight] = self.junction.get_traffic_lights()
+
+            for light in lights:
+                if light.id == self.current_traffic_light_id:
+                    light.state = True
+                else:
+                    light.state = False
+
+            self.current_traffic_light_id = self.get_next_traffic_light_id()
+
+            time.sleep(self.time_interval)
+
+    def get_next_traffic_light_id(self):
+        """
+        Get the id of next traffic light in the round-robin
+        """
+
+        lights: List[TrafficLight] = self.junction.get_traffic_lights()
+
+        for i, light in enumerate(lights):
+            if light.id == self.current_traffic_light_id:
+                return lights[(i + 1) % len(lights)].id
+
+    def get_current_state(self):
+        """
+        Get the current state of the traffic lights - red or green
+        """
+
+        return self.junction.get_traffic_lights()
